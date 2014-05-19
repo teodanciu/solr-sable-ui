0e5264f5973a5c970827207b06184d248f3176cb
teo <teo.danciu@gmail.com>
2014-05-19 19:33:50 +0100

    Run query with user-input solr core.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,18 +13,24 @@ myAppModule.controller("SolrController",
         $scope.hosts = [
             "localhost"
         ];
+        $scope.cores = [
+            "uk_en",
+            "us_en"
+        ];
 
         var parsedUrl = jQuery.url(window.location);
         var initialQuery = parsedUrl.param("query") || $scope.queries[0];
         var initialHost = parsedUrl.param("host") || $scope.hosts[0];
+        var initialCore = parsedUrl.param("core") || $scope.cores[0];
 
         clearState();
 
 //        var userId = Math.floor(Math.random()*10 + 1);
 //        $http.get("http://jsonplaceholder.typicode.com/posts?userId="+ userId).
-        $scope.makeSolrRequest = function(query, host) {
+        $scope.makeSolrRequest = function(query, host, core) {
             pushDistinct(query, $scope.queries);
             pushDistinct(host, $scope.hosts);
+            pushDistinct(core, $scope.cores);
 
             var params = jQuery.param({
                 q: query,
@@ -34,14 +40,17 @@ myAppModule.controller("SolrController",
                 indent: true
             });
 
-            $http.jsonp("http://" + host + ":8983/solr/collection1/select?" + params)
+            $http.jsonp("http://" + host + ":8983/solr/" + core + "/select?" + params)
               .success(function (data, status, headers, config){
+                    $scope.previous.host = $scope.current.host;
+                    $scope.previous.core = $scope.current.core;
+                    $scope.previous.query = $scope.current.query;
                     $scope.previous.data = $scope.current.data;
                     $scope.previous.title = $scope.current.title;
-                    $scope.previous.query = $scope.current.query;
-                    $scope.previous.host = $scope.current.host;
+
                     $scope.current.data = data.response;
-                    $scope.current.title = $scope.current.query + " on " + $scope.current.host;
+                    $scope.current.title = $scope.current.query + " on " + $scope.current.host + "/" + $scope.current.core;
+
 
                     $.JSONView(data.response, 'jsonoutput');
                     $.JSONView($scope.previous.data, 'jsonoutput-previous');
@@ -57,15 +66,17 @@ myAppModule.controller("SolrController",
 
         function clearState() {
             $scope.current = {
+                host: initialHost,
+                core: initialCore,
                 query: initialQuery,
                 data: "",
-                host: initialHost,
                 title: ""
             };
             $scope.previous = {
+                host: "",
+                core: "",
                 query: "",
                 data: "",
-                host: "",
                 title: ""
             };
         }
